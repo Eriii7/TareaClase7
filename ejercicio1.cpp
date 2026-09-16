7fe71c21ad8f59c4136a1c93f28a9ca2a88dd562
Erika Tutillo <erikatutillo446@gmail.com>
2026-09-15 21:41:22 -0500

    Semana 3-Funciones

---
+++ ejercicio1.cpp
@@ -0,0 +1,10 @@
+/*
+Autor : Erika Tutillo
+EJERCICIO 1 - SUMA Y PROMEDIO DE 3 NUMEROS
+Desarrolla un programa que permita ingresar 3 números enteros y que use funciones
+para:
+• leer los números,
+• calcular la suma,
+• calcular el promedio,
+• mostrar los resultados.
+*/
